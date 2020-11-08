9e4fbbd336ad14ce3c0b494271447617adae7a25
HubiDev <lukas.huber7@gmail.com>
2020-11-08 16:58:08 +0100

    Added copyright headers

--- src/game/src/elements/plane.cpp
+++ src/game/src/elements/plane.cpp
@@ -1,3 +1,20 @@
+// ------------------------------------------------------------------------
+// This file is part of planecontrol.
+
+// planecontrol is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// planecontrol is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with planecontrol.  If not, see <http://www.gnu.org/licenses/>.
+// ------------------------------------------------------------------------
+
 #include "game/elements/plane.hpp"
 #include "core/engine/update_context.hpp"
 #include "core/graphics/geometry.hpp"
@@ -67,7 +84,7 @@ void Plane::onMouseUp(const core::ui::MouseEventArgs& f_eventArgs)
         {
             m_flightTrack_p->clear();
         }
-        
+
         m_flightTrack_p->setActive(false);
     }
     else
