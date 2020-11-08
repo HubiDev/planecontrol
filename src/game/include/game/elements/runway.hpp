cc7bf960864304ee571acd743bf4e0df19264636
HubiDev <lukas.huber7@gmail.com>
2020-11-08 15:49:13 +0100

    Started with runway implementation

---
+++ src/game/include/game/elements/runway.hpp
@@ -0,0 +1,19 @@
+#ifndef GAME_ELEMENTS_RUWAY_HPP_INCLUDED
+#define GAME_ELEMENTS_RUWAY_HPP_INCLUDED
+
+#include "core/engine/game_element.hpp"
+
+namespace game
+{
+namespace elements
+{
+class Runway : public core::engine::IGameElement
+{
+public:
+    Runway();
+};
+
+} // namespace elements
+} // namespace game
+
+#endif
---
+++ src/game/src/elements/runway.cpp
@@ -0,0 +1,9 @@
+#include "game/elements/runway.hpp"
+
+namespace game
+{
+namespace elements
+{
+Runway::Runway(/* args */) {}
+} // namespace elements
+} // namespace game
